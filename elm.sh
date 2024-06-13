10aeae054c0a876be6b0bdaa79e05d825c755650
ShuyinYang <103890056+Shuying-exquisite@users.noreply.github.com>
2024-06-13 14:59:23 +0800

    Create elm.sh

---
+++ elm.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+export elmck="your_value_here"
+
+# 调用乐园币脚本
+node ./ele_lyb.js
+
+# 延时
+delay=$(( ($RANDOM % 6) + 5 ))
+echo "延时 $delay 秒以防止黑号..."
+sleep $delay
+
+# 调用2048脚本
+node ./ele_2048.js
+
+# 延时
+delay=$(( ($RANDOM % 6) + 5 ))
+echo "延时 $delay 秒以防止黑号..."
+sleep $delay
+
+# 调用成语闯关脚本
+node ./ele_cycg.js
+
+# 延时
+delay=$(( ($RANDOM % 6) + 5 ))
+echo "延时 $delay 秒以防止黑号..."
+sleep $delay
+
+# 调用美食摊脚本
+node ./ele_mst.js
+
+# 延时
+delay=$(( ($RANDOM % 6) + 5 ))
+echo "延时 $delay 秒以防止黑号..."
+sleep $delay
+
+# 调用合成甜蜜蜜脚本
+node ./ele_hctmm.js
+
+# 延时
+delay=$(( ($RANDOM % 6) + 5 ))
+echo "延时 $delay 秒以防止黑号..."
+sleep $delay
+
+# 调用饿了么资产推送脚本
+node ./饿了么资产推送.js
+
+# 延时
+delay=$(( ($RANDOM % 6) + 5 ))
+echo "延时 $delay 秒以防止黑号..."
+sleep $delay
