7aff4687f366e6575cff2aa7c77cd651a23c58c0
ShuyinYang <103890056+Shuying-exquisite@users.noreply.github.com>
2024-06-13 15:04:20 +0800

    Update elm.py

--- elm.py
+++ elm.py
@@ -2,20 +2,25 @@ import streamlit as st
 import subprocess
 
 # 定义执行Shell脚本的函数
-def run_shell_script():
+def run_shell_script(elmck):
     # 执行Shell脚本
-    result = subprocess.run(["./elm.sh"], capture_output=True, text=True, shell=True)
-    
+    result = subprocess.run(["./elm.sh", elmck], capture_output=True, text=True)
     return result.stdout, result.stderr
 
 # Streamlit界面
 st.title("饿了么自动化脚本")
 
+# 用户输入elmck
+elmck = st.text_input("请输入elmck值:")
+
 if st.button("执行脚本"):
-    st.write("脚本正在执行，请稍候...")
-    stdout, stderr = run_shell_script()
-    st.write("脚本输出:")
-    st.text(stdout)
-    if stderr:
-        st.write("脚本错误:")
-        st.text(stderr)
+    if elmck:
+        st.write("脚本正在执行，请稍候...")
+        stdout, stderr = run_shell_script(elmck)
+        st.write("脚本输出:")
+        st.text(stdout)
+        if stderr:
+            st.write("脚本错误:")
+            st.text(stderr)
+    else:
+        st.write("请输入elmck值")
