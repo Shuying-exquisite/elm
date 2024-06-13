ce3f7be764f540aec1940234afb8bc3528bd398c
ShuyinYang <103890056+Shuying-exquisite@users.noreply.github.com>
2024-06-13 15:21:07 +0800

    Update elm.py

--- elm.py
+++ elm.py
@@ -2,13 +2,23 @@ import streamlit as st
 import subprocess
 import os
 
+# 添加执行权限
+def add_execute_permission(script_path):
+    try:
+        os.chmod(script_path, 0o755)  # 添加执行权限
+        st.write("已成功添加执行权限")
+    except Exception as e:
+        st.write(f"添加执行权限时出现错误：{e}")
+
 # 定义执行Shell脚本的函数
 def run_shell_script(elmck):
     try:
         script_path = os.path.join(os.path.dirname(__file__), "elm.sh")
+        add_execute_permission(script_path)  # 添加执行权限
         
         # 调试信息
         st.write(f"当前工作目录: {os.getcwd()}")
+        st.write(f"__file__: {__file__}")
         st.write(f"脚本路径: {script_path}")
         st.write(f"文件存在: {os.path.isfile(script_path)}")
         st.write(f"文件可执行: {os.access(script_path, os.X_OK)}")
@@ -36,3 +46,4 @@ if st.button("执行脚本"):
             st.text(stderr)
     else:
         st.write("请输入elmck值")
+
