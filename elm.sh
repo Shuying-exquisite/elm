1e1573451b57e1a844e84901537c4b982101ee95
ShuyinYang <103890056+Shuying-exquisite@users.noreply.github.com>
2024-08-18 18:29:19 +0800

    Update elm.sh

--- elm.sh
+++ elm.sh
@@ -1,49 +1,12 @@
 #!/bin/sh
-export elmck=$1
-# 调用乐园币脚本
-node ./ele_lyb.js
+# 饿了么中央控制脚本
 
-# 延时
+#node ./脚本名称.js
+#python ./脚本名称.py
+
+# 延时防黑
 delay=$(( ($RANDOM % 6) + 5 ))
 echo "延时 $delay 秒以防止黑号..."
 sleep $delay
 
-# 调用2048脚本
-node ./ele_2048.js
 
-# 延时
-delay=$(( ($RANDOM % 6) + 5 ))
-echo "延时 $delay 秒以防止黑号..."
-sleep $delay
-
-# 调用成语闯关脚本
-node ./ele_cycg.js
-
-# 延时
-delay=$(( ($RANDOM % 6) + 5 ))
-echo "延时 $delay 秒以防止黑号..."
-sleep $delay
-
-# 调用美食摊脚本
-node ./ele_mst.js
-
-# 延时
-delay=$(( ($RANDOM % 6) + 5 ))
-echo "延时 $delay 秒以防止黑号..."
-sleep $delay
-
-# 调用合成甜蜜蜜脚本
-node ./ele_hctmm.js
-
-# 延时
-delay=$(( ($RANDOM % 6) + 5 ))
-echo "延时 $delay 秒以防止黑号..."
-sleep $delay
-
-# 调用饿了么资产推送脚本
-node ./饿了么资产推送.js
-
-# 延时
-delay=$(( ($RANDOM % 6) + 5 ))
-echo "延时 $delay 秒以防止黑号..."
-sleep $delay
